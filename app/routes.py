768d075eadbc1cb0140a715d0d6fdc483ce944a3
Karla Nunez <karla.nunez@redfin.com>
2019-05-11 14:35:27 -0700

    mocks of some endpoints we want to implement

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,16 @@ from app import app
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+
+@app.route('/cellar')
+def cellar():
+    return "This is a mock of what you have on your cellar, stay tuned"
+
+@app.route('/cellar/breweries')
+def breweries():
+    return "This is a mock of all the unique breweries you have"
+
+@app.route('/cellar/brewery/<name>')
+def brewery(name):
+    return "This is a mock of everything we know about brewery " + name
